128db345d4499a37c82cdd7fb7cc7bee8d1d7ec1
R3m0tEe <bill110179@gmail.com>
2022-11-11 14:42:25 +0200

    Closes #31 - Get nearby Sights, Cafes and Hospitals via the Google Places API matching geolocation

--- server/helpers/urlParser.js
+++ server/helpers/urlParser.js
@@ -1,6 +1,7 @@
 // constructing url for each image, given required parameters
 const urlParser = function (params) {
-    return `https://live.staticflickr.com/${serverId}/${photoId}_${secret}.jpg`;
+    const { server, id, secret } = params;
+    return `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`;
 };
 
 module.exports = urlParser;
