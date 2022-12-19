67aa27b416c2fc90e6c208d1f3be1c3dfc3d0b55
R3m0tEe <bill110179@gmail.com>
2022-12-19 15:51:06 +0200

    added test for flickr api

---
+++ server/helpers/urlParser.test.js
@@ -0,0 +1,16 @@
+import { it, expect } from 'vitest';
+import urlParser from './urlParser';
+
+it('should return Flicker image url', () => {
+   const params = {
+      server: 'a',
+      id: 'b',
+      secret: 'c'
+   };
+
+   const resultedUrl = 'https://live.staticflickr.com/a/b_c.jpg';
+
+   const result = urlParser(params);
+
+   expect(result).toBe(resultedUrl);
+});
